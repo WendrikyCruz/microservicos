59c1aa561869b978fb16a3a57217f11ab89c8c06
Wendriky Cruz <wendriky.cruz@outlook.com>
2020-11-27 20:18:40 -0300

    Project hr-config-server created

---
+++ hr-config-srver/src/main/java/com/microservicehrconfigserver/HrConfigSrverApplication.java
@@ -0,0 +1,13 @@
+package com.microservicehrconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrConfigSrverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrConfigSrverApplication.class, args);
+	}
+
+}
---
+++ hr-config-srver/src/test/java/com/microservicehrconfigserver/HrConfigSrverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicehrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigSrverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
