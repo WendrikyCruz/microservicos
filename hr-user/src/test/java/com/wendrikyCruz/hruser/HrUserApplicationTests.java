6adfcd950c32dfecc0c843d177c70a67e242b467
Wendriky Cruz <wendriky.cruz@outlook.com>
2020-12-02 20:59:46 -0300

    Project hr-user created

---
+++ hr-user/src/test/java/com/wendrikyCruz/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wendrikyCruz.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
