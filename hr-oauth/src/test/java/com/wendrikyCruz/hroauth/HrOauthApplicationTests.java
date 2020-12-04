a610d5c912921faaf9caee201880d6507d78969b
Wendriky Cruz <wendriky.cruz@outlook.com>
2020-12-04 14:29:41 -0300

    Project hr-oauth created

---
+++ hr-oauth/src/test/java/com/wendrikyCruz/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wendrikyCruz.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
